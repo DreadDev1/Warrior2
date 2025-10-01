e5df846b0327945b9cb6df91c29954fac96f9546
DreadDev1 <kameen.avery@gmail.com>
2025-09-30 22:39:15 -0400

    Section 2 Lecture 46 Warrior Function Library

---
+++ Source/Warrior/Private/ARPG_FunctionLibrary.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ARPG_FunctionLibrary.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Game/AbilitySystem/ARPG_AbilitySystemComponent.h"
+
+UARPG_AbilitySystemComponent* UARPG_FunctionLibrary::NativeGetARPG_ASCFromActor(AActor* InActor)
+{
+	check(InActor);
+
+	return CastChecked<UARPG_AbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(InActor));
+}
+
+void UARPG_FunctionLibrary::AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag TagToAdd)
+{
+	UARPG_AbilitySystemComponent* ASC = NativeGetARPG_ASCFromActor(InActor);
+
+	if (!ASC->HasMatchingGameplayTag(TagToAdd))
+	{
+		ASC->AddLooseGameplayTag(TagToAdd);
+	}
+}
+
+void UARPG_FunctionLibrary::RemoveGameplayFromActorIfFound(AActor* InActor, FGameplayTag TagToRemove)
+{
+	UARPG_AbilitySystemComponent* ASC = NativeGetARPG_ASCFromActor(InActor);
+
+	if (ASC->HasMatchingGameplayTag(TagToRemove))
+	{
+		ASC->RemoveLooseGameplayTag(TagToRemove);
+	}
+}
+
+bool UARPG_FunctionLibrary::NativeDoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck)
+{
+	UARPG_AbilitySystemComponent* ASC = NativeGetARPG_ASCFromActor(InActor);
+
+	return ASC->HasMatchingGameplayTag(TagToCheck);
+}
+
+void UARPG_FunctionLibrary::BP_DoesActorHaveTag(AActor* InActor, FGameplayTag TagToCheck, EARPG_ConfirmType& OutConfirmType)
+{
+	OutConfirmType = NativeDoesActorHaveTag(InActor,TagToCheck)? EARPG_ConfirmType::Yes : EARPG_ConfirmType::No;
+}
---
+++ Source/Warrior/Public/ARPG_FunctionLibrary.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ARPG_FunctionLibrary.generated.h"
+
+class UARPG_AbilitySystemComponent;
+
+UENUM()
+enum class EARPG_ConfirmType : uint8
+{
+	Yes,
+	No
+};
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UARPG_FunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	static UARPG_AbilitySystemComponent* NativeGetARPG_ASCFromActor(AActor* InActor);
+
+	UFUNCTION(BlueprintCallable, Category = "ARPG_|FunctionLibrary")
+	static void AddGameplayTagToActorIfNone(AActor* InActor,FGameplayTag TagToAdd);
+
+	UFUNCTION(BlueprintCallable, Category = "ARPG_|FunctionLibrary")
+	static void RemoveGameplayFromActorIfFound(AActor* InActor,FGameplayTag TagToRemove);
+
+	static bool NativeDoesActorHaveTag(AActor* InActor,FGameplayTag TagToCheck);
+
+	UFUNCTION(BlueprintCallable, Category = "ARPG_|FunctionLibrary", meta = (DisplayName = "Does Actor Have Tag", ExpandEnumAsExecs = "OutConfirmType"))
+	static void BP_DoesActorHaveTag(AActor* InActor,FGameplayTag TagToCheck,EARPG_ConfirmType& OutConfirmType);
+};
